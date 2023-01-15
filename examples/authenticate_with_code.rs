50d7a9083af74488c935b7b11448c42babcbc906
Sammwy <sammwy.dev@gmail.com>
2023-01-15 05:59:34 -0300

    doc: 📙 refactorized example file

--- examples/authenticate_with_code.rs
+++ examples/authenticate_with_code.rs
@@ -2,13 +2,21 @@ use mc_auth::AuthFlow;
 
 fn main() {
     let mut auth = AuthFlow::new("9c1f1f43-58d5-4b7a-af0d-4e487f073441");
-    let data = auth.request_code().unwrap();
+    let code_res = auth.request_code().unwrap();
 
     println!(
         "Open this link in your browser {} and enter the following code: {}\nWaiting authentication...",
-        data.verification_uri, data.user_code
+        code_res.verification_uri, code_res.user_code
     );
+    auth.wait_for_login().unwrap();
 
-    let response = auth.wait_for_login().unwrap();
-    println!("Access token: {}", response.access_token);
+    println!("Logging in xbox live services...");
+    auth.login_in_xbox_live().unwrap();
+
+    println!("Logging in minecraft services...");
+    let minecraft = auth.login_in_minecraft().unwrap();
+
+    println!("Logged in:");
+    println!("Bearer token: {}", minecraft.access_token);
+    println!("UUID: {}", minecraft.username);
 }
